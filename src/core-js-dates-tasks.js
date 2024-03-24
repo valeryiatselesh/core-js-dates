2cdef6d865706e51361025d849c2011dd7c5bbb9
Valeryia Tselesh <valeryiatselesh@gmail.com>
2024-03-24 17:03:51 +0100

    feat: add getWorkSchedule solution

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -269,8 +269,29 @@ function getQuarter(date) {
  * { start: '01-01-2024', end: '15-01-2024' }, 1, 3 => ['01-01-2024', '05-01-2024', '09-01-2024', '13-01-2024']
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
-function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
-  throw new Error('Not implemented');
+function getWorkSchedule(period, countWorkDays, countOffDays) {
+  const schedule = [];
+
+  const startDate = new Date(period.start.split('-').reverse().join('-'));
+  const endDate = new Date(period.end.split('-').reverse().join('-'));
+
+  while (startDate <= endDate) {
+    for (
+      let workday = 0;
+      workday < countWorkDays && startDate <= endDate;
+      workday += 1
+    ) {
+      const day = startDate.getDate().toString().padStart(2, '0');
+      const month = (startDate.getMonth() + 1).toString().padStart(2, '0');
+      const year = startDate.getFullYear();
+
+      schedule.push(`${day}-${month}-${year}`);
+      startDate.setDate(startDate.getDate() + 1);
+    }
+
+    startDate.setDate(startDate.getDate() + countOffDays);
+  }
+  return schedule;
 }
 
 /**
